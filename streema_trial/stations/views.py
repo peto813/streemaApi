f93dfc37045b2658afce5fe9c70b4642c73a0b88
Einstein Millan <peto813@gmail.com>
2020-03-20 00:27:47 -0400

    created endpoint for related stations response

---
+++ streema_trial/stations/views.py
@@ -0,0 +1,19 @@
+import json
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from django.templatetags.static import static
+from django.conf import settings
+
+# Create your views here.
+class RelatedStationsView(APIView):
+    # queryset = models.Currency.objects.all()
+    # serializer_class = CurrencySerializer
+    permission_classes= (AllowAny,)
+    def get(self, request):
+        with open(settings.STATIC_ROOT+'/related_stations.json', 'r') as f:
+            distros_dict = json.load(f)
+        # return Response(distros_dict, status=status.HTTP_200_OK)
+        return Response(distros_dict, status=status.HTTP_200_OK)
